78e2c863a13c5a708f59d7790b83d9dcbedaf09d
HakimHad <haddoucheh@gmail.com>
2021-11-09 01:09:17 +0100

    pret a la production

--- app.js
+++ app.js
@@ -1,80 +1,84 @@
-const input = document.querySelector(".input-keep");
-const btnSubmit = document.querySelector(".btn-submit");
-let ul = document.querySelector("ul");
+const form = document.querySelector("form");
+
+var input = document.querySelector("input");
+const list = document.getElementById("list");
+const li = document.querySelectorAll('li')
+const p = document.createElement("p");
+const item = document.getElementsByClassName("item");
+const main = document.getElementById("main");
+
+
+
 let div = document.querySelector("div");
-let p = document.createElement("p");
+listItem = document.getElementsByClassName('list-item')
+const checkBtn = document.getElementsByClassName('check-btn')
+// debugger
+let inputLength = input.value.length
 input.focus();
+// noItemMsg()
 
-
-function noItemMsg(){
-    p.appendChild(document.createTextNode("Aucune tache pour le moment!"));
-    div.appendChild(p);
-    p.classList.add("paragraphe");
-//    console.log(p);
+//affichage du message "pas de tache"
+function noItemMsg() {
+    if (list.childElementCount  === 0){
+       p.appendChild(document.createTextNode("You are up to date ;-)"));
+       div.appendChild(p);
+       p.classList.add("paragraphe");
+       }
 }
 
-// function deleteNoItemMsg(){
-//     p
-// }
-
-
-(function getValues(){
-    // debugger
-    
-    if(!window.localStorage.toDoList) {
-       noItemMsg()
-    }else{
-        ul.innerHTML  = window.localStorage.toDoList;
-    }
-})()
-
-
-
-function createNewKeep() {
-    let li = document.createElement("li");
-    let delBtn = document.createElement("button")
-    let checkBtn = document.createElement("button")
-    // p.classList.add("paragraphe-hidden");
-    
-    p.style.display = "none";
-    
-    checkBtn.appendChild(document.createTextNode("V"));
-    li.appendChild(checkBtn);
-    checkBtn.classList.add("check-btn")
-    // checkBtn.addEventListener("click", checkItem())
-
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    li.classList.add("list-item")
-
-    delBtn.appendChild(document.createTextNode("X"));
-    li.appendChild(delBtn);
-    delBtn.classList.add("del-btn")
-    // delBtn.addEventListener("click", deleteItem())
-
+form.addEventListener("submit",(e =>{
+    e.preventDefault();
+    list.innerHTML += `<div><span>${input.value} </span><span class="trash-btn material-icons ">delete</span></li></div>`;
     input.value = "";
-    
-    // function checkItem(){
-    //     li.classList.add("done")
-    // }
-
-
-    // function deleteItem(){
-    //     li.classList.add("disappear")
-    // }
-
-
-
+    p.textContent ="";
+    p.remove();
 
     storage();
-    input.focus();
-    
-}
+    })
+)
 
+        // Injection/suppression class "done"
+list.addEventListener('click', (e) =>{
+    const listParent = e.target.parentElement;
+        
+    if (e.target.classList[0] === "trash-btn"){
+        listParent.remove();
+        storage();
+        if (main.children !=p ){
+            noItemMsg()};
+
+    }if (listParent === list){
+        e.target.classList.toggle('done-div');
+        e.target.firstChild.classList.toggle('done-text');
+        // 
+        return;
+
+    }else{
+        e.target.parentElement.classList.toggle('done-div');
+        e.target.classList.toggle('done-text');
+        input.value = "";
+        storage();
+    }
+})
+
+    // mise en memoire
 function storage() {
-  window.localStorage.toDoList = ul.innerHTML;
+  window.localStorage.toDoList = list.innerHTML;
+}
+ //affichage du message "pas de tache"
+ function noItemMsg() {
+     if (list.childElementCount  === 0){
+        p.appendChild(document.createTextNode("You are up to date! ;-)"));
+        div.appendChild(p);
+        p.classList.add("paragraphe");
+        }
 }
 
-
-
-
+//recupere les données en memoire
+(function getValues() { 
+    if (!window.localStorage.toDoList) {
+      noItemMsg();
+    } else {
+      list.innerHTML = window.localStorage.toDoList;
+    }
+  })()
