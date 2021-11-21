695e65fbc08ef8fa6f2d078880d8b8b31d9c477b
HakimHad <haddoucheh@gmail.com>
2021-11-21 22:44:37 +0100

    first commit

--- app.js
+++ app.js
@@ -1,84 +1,34 @@
-const form = document.querySelector("form");
-
-var input = document.querySelector("input");
-const list = document.getElementById("list");
-const li = document.querySelectorAll('li')
-const p = document.createElement("p");
-const item = document.getElementsByClassName("item");
-const main = document.getElementById("main");
+const contents = document.querySelectorAll('.content');
+const tabs = document.querySelectorAll('.tab');
+let index = 0
 
 
 
-let div = document.querySelector("div");
-listItem = document.getElementsByClassName('list-item')
-const checkBtn = document.getElementsByClassName('check-btn')
-// debugger
-let inputLength = input.value.length
-input.focus();
-// noItemMsg()
 
-//affichage du message "pas de tache"
-function noItemMsg() {
-    if (list.childElementCount  === 0){
-       p.appendChild(document.createTextNode("You are up to date ;-)"));
-       div.appendChild(p);
-       p.classList.add("paragraphe");
-       }
-}
-
-form.addEventListener("submit",(e =>{
-    e.preventDefault();
-    list.innerHTML += `<div><span>${input.value} </span><span class="trash-btn material-icons ">delete</span></li></div>`;
-    input.value = "";
-    p.textContent ="";
-    p.remove();
-
-    storage();
-    })
-)
-
-        // Injection/suppression class "done"
-list.addEventListener('click', (e) =>{
-    const listParent = e.target.parentElement;
-        
-    if (e.target.classList[0] === "trash-btn"){
-        listParent.remove();
-        storage();
-        if (main.children !=p ){
-            noItemMsg()};
-
-    }if (listParent === list){
-        e.target.classList.toggle('done-div');
-        e.target.firstChild.classList.toggle('done-text');
-        // 
-        return;
-
-    }else{
-        e.target.parentElement.classList.toggle('done-div');
-        e.target.classList.toggle('done-text');
-        input.value = "";
-        storage();
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        if(tab.classList.contains('active')){
+            return;
+        }else{
+            tab.classList.add('active')
+        }        
+        index = tab.getAttribute('data-anim')
+        // console.log(index);
+        for (i=0; i<tabs.length; i++){
+            if (tabs[i].getAttribute('data-anim') != index){
+                tabs[i].classList.remove('active');
+            }
+        }
+        for (j=0; j<contents.length; j++){
+            if (contents[j].getAttribute('data-anim') != index){
+                contents[j].classList.add('hide-content');
+            }else {
+                contents[j].classList.remove('hide-content');
+            }
+        }
     }
+)
 })
 
-    // mise en memoire
-function storage() {
-  window.localStorage.toDoList = list.innerHTML;
-}
- //affichage du message "pas de tache"
- function noItemMsg() {
-     if (list.childElementCount  === 0){
-        p.appendChild(document.createTextNode("You are up to date! ;-)"));
-        div.appendChild(p);
-        p.classList.add("paragraphe");
-        }
-}
 
-//recupere les données en memoire
-(function getValues() { 
-    if (!window.localStorage.toDoList) {
-      noItemMsg();
-    } else {
-      list.innerHTML = window.localStorage.toDoList;
-    }
-  })()
+
